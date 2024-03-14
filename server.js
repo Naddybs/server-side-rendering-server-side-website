dd450812b64b54e29107d62e08c786b4a9567795
Naddybs <144009698+Naddybs@users.noreply.github.com>
2024-03-14 06:22:48 +0100

    layout opzet

--- server.js
+++ server.js
@@ -26,43 +26,24 @@ app.set('views', './views')
 // De public map bevat bestanden die je kunt gebruiken in de html pagina's
 app.use(express.static('public'))
 
+// dit zorgt ervoor dat het werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
 // 6. hiermee render je de index.ejs pagina
-// De server stuurt de index.ejs pagina terug naar de client
-// De client ontvangt de pagina en ziet de html en css
+// De server stuurt de index.ejs pagina terug naar de client, de client ontvangt de pagina en ziet de html en css
+// De client ziet de data van de API in de html pagina
 app.get('/', function (req, res) {
     fetchJson(apiUrl).then((apiData)  => {
         res.render('index', {houses: apiData.data})
-        // console.log(apiData.data)
     })
 })
-
-
-// dit zorgt ervoor dat het werken met request data makkelijker wordt
-    app.use(express.urlencoded({extended: true}))
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 // 3. Hiermee maak je een nieuwe server op poort 3000
 // De ('app') server luistert naar requests op poort 3000
-// De server reageert op requests met de juiste route
-// De server stuurt een response terug naar de client
+// De server reageert op requests met de juiste route, de server stuurt een response terug naar de client
 app.set('port', process.env.PORT || 3000)
-
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
